a02df71c956b6707104841e77125db84abe14312
carlzhang4 <2470871694@qq.com>
2022-06-14 21:11:01 +0800

    baseline : multi threads and multi iterations support

--- baseline/main.cpp
+++ baseline/main.cpp
@@ -74,9 +74,7 @@ int init_bufs(int node_id, void** bufs, void* verify_buf, int* compressed_length
 	LOG_I("Init buf done");
 	return num_chunck;
 }
-void sub_task_server(int thread_index, QpHandler* handler, void* buf, void* verify_buf, size_t buf_size, int num_chunck, int chunck_size, int iterations){
-	size_t ops = iterations * num_chunck;
-
+void sub_task_server(int thread_index, QpHandler* handler, void* buf, void* verify_buf, size_t buf_size, int num_chunck, int chunck_size, size_t ops){
 	int ne_send;
 	int ne_recv;
 	struct ibv_wc *wc_send = NULL;
@@ -145,9 +143,7 @@ void sub_task_server(int thread_index, QpHandler* handler, void* buf, void* veri
 	LOG_I("Time : %.3f s",duration);
 	LOG_I("Speed : %.2f Gb/s",8.0*ops*chunck_size/1024/1024/1024/duration);
 }
-void sub_task_client(int thread_index, QpHandler* handler, void* buf, void* verify_buf, int* compressed_lengths, size_t buf_size, int num_chunck, int chunck_size, int iterations){
-	size_t ops = iterations * num_chunck;
-
+void sub_task_client(int thread_index, QpHandler* handler, void* buf, void* verify_buf, int* compressed_lengths, size_t buf_size, int num_chunck, int chunck_size, size_t ops){
 	int ne_send;
 	int ne_recv;
 	struct ibv_wc *wc_send = NULL;
@@ -223,11 +219,14 @@ void compression_benchmark(NetParam &net_param, int num_threads, int iterations,
 	assert(num_threads<=num_cpus);
 	
 	//init bufs and verify buf
-	size_t buf_size = 256*1024*1024;
+	size_t buf_size = 128*1024*1024;
 	void** bufs = new void*[num_threads];
 	void* verify_buf = myMalloc2MbPage(buf_size/2);
 	int* compressed_lengths = new int[buf_size/2/chunck_size];
 	int num_chunck = init_bufs(net_param.nodeId,bufs,verify_buf,compressed_lengths,num_threads,buf_size,chunck_size);
+	size_t ops = size_t(1) * iterations * num_chunck;
+	LOG_I("%-20s : %d","Iterations",iterations);
+	LOG_I("%-20s : %d","Ops",ops);
 
 	//connect all qps
 	PingPongInfo *info = new PingPongInfo[net_param.numNodes*num_threads]();
@@ -245,16 +244,14 @@ void compression_benchmark(NetParam &net_param, int num_threads, int iterations,
 		connect_qp_rc(net_param,*qp_handlers[i],info+dest_id*num_threads+i,info+my_id*num_threads+i);
 	}
 
-	size_t ops = iterations * num_chunck;
-
 	vector<thread> threads(num_threads);
 	struct timespec start_timer,end_timer;
 	clock_gettime(CLOCK_MONOTONIC, &start_timer);
 	for(int i=0;i<num_threads;i++){
 		if(net_param.nodeId == 0){
-			threads[i] = thread(sub_task_server, i, qp_handlers[i], bufs[i], verify_buf, buf_size, num_chunck, chunck_size, iterations);
+			threads[i] = thread(sub_task_server, i, qp_handlers[i], bufs[i], verify_buf, buf_size, num_chunck, chunck_size, ops);
 		}else if(net_param.nodeId == 1){
-			threads[i] = thread(sub_task_client, i, qp_handlers[i], bufs[i], verify_buf, compressed_lengths, buf_size, num_chunck, chunck_size, iterations);
+			threads[i] = thread(sub_task_client, i, qp_handlers[i], bufs[i], verify_buf, compressed_lengths, buf_size, num_chunck, chunck_size, ops);
 		}
 		set_cpu(threads[i],i);
 	}
@@ -267,141 +264,13 @@ void compression_benchmark(NetParam &net_param, int num_threads, int iterations,
 	LOG_I("Total Speed : %.2f Gb/s",8.0*num_threads*ops*chunck_size/1024/1024/1024/duration);
 }
 int main(int argc, char *argv[]){
+	int num_threads = 48;
+	int iterations = 500;
+	int chunck_size = 16*1024;
 	NetParam net_param;
 	get_opt(net_param,argc,argv);
 	socket_init(net_param);
-    roce_init(net_param);
-	compression_benchmark(net_param, 1, 1, 4*1024);
-
-	// int num_threads = 1;
-	// int ops = 10;
-	// int pack_size = 64;
-	// size_t buf_size = 128*1024*1024;
-	// PingPongInfo *info = new PingPongInfo[net_param.numNodes*num_threads]();
-	// void** bufs = new void*[num_threads];
-	// QpHandler** qp_handlers = new QpHandler*[num_threads]();
-	// for(int i=0;i<num_threads;i++){
-	// 	bufs[i] = myMalloc2MbPage(buf_size);
-	// 	for(int j=0;j<buf_size/sizeof(int);j++){
-	// 		if(net_param.nodeId == 0){
-	// 			((int**)bufs)[i][j] = j;
-	// 		}else{
-	// 			((int**)bufs)[i][j] = 0;
-	// 		}
-	// 	}
-	// }
-	// for(int i=0;i<num_threads;i++){
-	// 	qp_handlers[i] = create_qp_rc(net_param,bufs[i],buf_size,info+i);
-	// }
-	// exchange_data(net_param, (char*)info, sizeof(PingPongInfo)*num_threads);
-	// for(int i=0;i<net_param.numNodes*num_threads;i++){
-	// 	print_pingpong_info(info+i);
-	// }
-	// int my_id = net_param.nodeId;
-	// int dest_id = (net_param.nodeId+1)%net_param.numNodes;
-	// for(int i=0;i<num_threads;i++){
-	// 	connect_qp_rc(net_param,*qp_handlers[i],info+dest_id*num_threads+i,info+my_id*num_threads+i);
-	// }
-
-	// int ne;
-	// struct ibv_wc *wc = NULL;
-	// ALLOCATE(wc ,struct ibv_wc ,CTX_POLL_BATCH);
-	// struct timespec start_timer,end_timer;
-	// int start_timer_flag = 1;
-	// if(net_param.nodeId == 0){
-	// 	int cur_send = 0;
-	// 	int cur_complete = 0;
-	// 	for(int i=0;i<min(qp_handlers[0]->tx_depth,ops);i++){
-	// 		post_send(*qp_handlers[0],(cur_send*pack_size)%buf_size,pack_size);
-	// 		cur_send+=1;
-	// 	}
-	// 	while(cur_complete<ops){
-	// 		ne = poll_send_cq(*qp_handlers[0],wc);
-	// 		if(start_timer_flag==1 && ne!=0){
-	// 			clock_gettime(CLOCK_MONOTONIC, &start_timer);
-	// 			start_timer_flag=0;
-	// 		}
-	// 		for(int i=0;i<ne;i++){
-	// 			if(cur_send<ops){
-	// 				post_send(*qp_handlers[0],(cur_send*pack_size)%buf_size,pack_size);
-	// 				cur_send+=1;
-	// 			}
-	// 			assert(wc[i].status == IBV_WC_SUCCESS);
-	// 		}
-	// 		cur_complete+=ne;
-	// 	}
-	// 	clock_gettime(CLOCK_MONOTONIC, &end_timer);
-	// }else{
-	// 	int cur_recv = 0;
-	// 	int cur_complete = 0;
-	// 	for(int i=0;i<min(qp_handlers[0]->rx_depth,ops);i++){
-	// 		post_recv(*qp_handlers[0],(cur_recv*pack_size)%buf_size,pack_size);
-	// 		cur_recv+=1;
-	// 	}
-	// 	while(cur_complete<ops){
-	// 		ne = poll_recv_cq(*qp_handlers[0],wc);
-	// 		if(start_timer_flag==1 && ne!=0){
-	// 			clock_gettime(CLOCK_MONOTONIC, &start_timer);
-	// 			start_timer_flag=0;
-	// 		}
-	// 		for(int i=0;i<ne;i++){
-	// 			if(cur_recv<ops){
-	// 				post_recv(*qp_handlers[0],(cur_recv*pack_size)%buf_size,pack_size);
-	// 				cur_recv+=1;
-	// 			}
-	// 			assert(wc[i].status == IBV_WC_SUCCESS);
-	// 			LOG_D("wc[%d].byte_len : %d",i,wc[i].byte_len);
-	// 			for(int j=0;j<wc[i].byte_len/sizeof(int);j++){
-	// 				int start_index = (cur_complete+i)*pack_size/sizeof(int);
-	// 				cout<<((int**)bufs)[0][start_index+j]<<" ";
-	// 			}
-	// 			cout<<endl;
-	// 		}
-	// 		cur_complete+=ne;
-	// 	}
-	// 	clock_gettime(CLOCK_MONOTONIC, &end_timer);
-
-	// 	int verify_data_size = (ops*pack_size)%buf_size;
-	// 	for(int i=0;i<verify_data_size/sizeof(int);i++){
-	// 		if(((int*)bufs[0])[i] != i){
-	// 			LOG_D("Data verification failed, index:%d data:%d expected_data:%d",i,((int*)bufs[0])[i],i);
-	// 			break;
-	// 		}
-	// 	}
-	// 	LOG_I("Data verification success!");
-	// }
-	// double duration = end_timer.tv_sec-start_timer.tv_sec+1.0*(end_timer.tv_nsec-start_timer.tv_nsec)/1e9;
-	// LOG_I("Time : %.3f s",duration);
-	// LOG_I("Speed : %.2f Gb/s",8.0*ops*pack_size/1024/1024/1024/duration);
-	
-	// ifstream ifs( "../../dataset/silesia/dickens", ifstream::binary );
-	// assert(ifs.is_open());
-	// ifs.seekg(0,ifs.end);
-	// int file_size = ifs.tellg();
-	// ifs.clear();
-	// ifs.seekg(0,ios::beg);
-
-	// char* data = new char[file_size];
-	// ifs.read(data,file_size);
-	// ifs.close();
-	
-	// int chunck_size = 4*1024;
-	// int num_chunck = (file_size+chunck_size-1)/chunck_size;
-
-	// for(int i=0;i<num_chunck;i++){
-	// 	int compressed_size = compress(data+i*chunck_size, ((char*)verify_buf)+i*chunck_size, chunck_size);
-	// }
-	// char *source_data = data;
-	// int source_size = file_size;
-	// char* compressed_data = new char[source_size];
-	// char* decompressed_data = new char[source_size];
-	// int compressed_size = compress(source_data, compressed_data, source_size);
-	// int decompressed_size = decompress(compressed_data,decompressed_data,compressed_size,source_size);
-
-	// cout<<"source_size       : "<<source_size<<endl;
-	// cout<<"compressed_size   : "<<compressed_size<<endl;
-	// cout<<"decompressed_size : "<<decompressed_size<<endl;
-	// cout<<"Compare result    : "<<strcmp(source_data,decompressed_data)<<endl;
-
+    roce_init(net_param,num_threads);
+	compression_benchmark(net_param, num_threads, iterations, chunck_size);
     return 0;
 }
